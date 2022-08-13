e7efbe2924ae2f60c8ccb9a50af2ba2c18a706c9
Jozef Hudec <jojohudec27@gmail.com>
2022-08-13 07:54:29 +0200

    Add basic drawing functionality

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
-const MAIN_GRID_WIDTH = 600
+const MAIN_GRID_WIDTH = 600;
 
-const outerGridContainerEl = document.querySelector('.outer-grid-container')
-outerGridContainerEl.style.height = `${MAIN_GRID_WIDTH}px`
-outerGridContainerEl.style.overflow = 'hidden'
+const outerGridContainerEl = document.querySelector('.outer-grid-container');
+outerGridContainerEl.style.height = `${MAIN_GRID_WIDTH}px`;
 
-const gridEl = document.getElementById('main-grid')
-gridEl.style.width = `${MAIN_GRID_WIDTH}px`
-gridEl.style.height = `${MAIN_GRID_WIDTH}px`
+const gridEl = document.getElementById('main-grid');
+gridEl.style.width = `${MAIN_GRID_WIDTH}px`;
+gridEl.style.height = `${MAIN_GRID_WIDTH}px`;
 
 
 let gridSize = +prompt('How big do you want the grid to be?');
 
-const newSquareWidth = MAIN_GRID_WIDTH / gridSize 
+const newSquareWidth = MAIN_GRID_WIDTH / gridSize; 
 
-gridSize *= gridSize
+gridSize *= gridSize;
 
 while (gridSize > 0) {
-    let newSquare = document.createElement('div')
-    newSquare.style.width = `${newSquareWidth}px`
-    newSquare.style.height = `${newSquareWidth}px`
-    newSquare.style.border = '1px solid black'
+    let newSquare = document.createElement('div');
+    newSquare.style.width = `${newSquareWidth}px`;
+    newSquare.style.height = `${newSquareWidth}px`;
+    newSquare.style.border = '1px solid black';
 
-    gridEl.appendChild(newSquare)
+    newSquare.addEventListener('mouseover', (e) => {
+        if (e.buttons) {
+            newSquare.style.backgroundColor = 'black';
+        }
+    });
+
+    newSquare.addEventListener('mousedown', () => {
+        newSquare.style.backgroundColor = 'black';
+    });
+
+    gridEl.appendChild(newSquare);
 
     gridSize--;
 };
