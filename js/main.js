20e2176f9a27c3743660997516bc32daaa2fd47d
Jozef Hudec <jojohudec27@gmail.com>
2022-08-13 08:30:54 +0200

    Add settings panel

--- js/main.js
+++ js/main.js
@@ -7,8 +7,25 @@ const gridEl = document.getElementById('main-grid');
 gridEl.style.width = `${MAIN_GRID_WIDTH}px`;
 gridEl.style.height = `${MAIN_GRID_WIDTH}px`;
 
+const settingsPanelEl = document.getElementById('settings-panel')
+settingsPanelEl.style.height = `${MAIN_GRID_WIDTH}px`;
+settingsPanelEl.style.width = `${MAIN_GRID_WIDTH/3}px`;
 
-let gridSize = +prompt('How big do you want the grid to be?');
+let tiles;
+const tileBorderCheck = document.getElementById('toggle-tile-border')
+tileBorderCheck.addEventListener('change', e => {
+    if (!tiles) {
+        tiles = document.getElementsByClassName('tile') 
+    };
+
+    let check = e.target.checked
+
+    Array.from(tiles).forEach(tile => {
+        tile.style.border = check ? '1px solid black' : ''
+    })
+})
+
+let gridSize = +prompt('How big do you want the grid to be?') || 8;
 
 const newSquareWidth = MAIN_GRID_WIDTH / gridSize; 
 
@@ -19,6 +36,7 @@ while (gridSize > 0) {
     newSquare.style.width = `${newSquareWidth}px`;
     newSquare.style.height = `${newSquareWidth}px`;
     newSquare.style.border = '1px solid black';
+    newSquare.classList.add('tile')
 
     newSquare.addEventListener('mouseover', (e) => {
         if (e.buttons) {
