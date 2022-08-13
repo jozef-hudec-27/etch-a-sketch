1f4b7ea418e46dfef6a9e0fb059d95adcfcff99e
Jozef Hudec <jojohudec27@gmail.com>
2022-08-13 11:26:08 +0200

    Add clear grid button and random color mode button

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ settingsPanelEl.style.width = `${MAIN_GRID_WIDTH/3}px`;
 let penColor = 'black';
 let bgColor = 'white';
 let isErasing = false;
+let randomColorMode = false;
 
 let tiles;
 const tileBorderCheck = document.getElementById('toggle-tile-border')
@@ -64,6 +65,8 @@ bgColorPicker.addEventListener('input', e => {
     const newBgColor = e.target.value
     bgColor = newBgColor
 
+    document.getElementById('clear-grid-btn').style.backgroundColor = bgColor
+
     const currentTiles = document.getElementsByClassName('tile') ;
     Array.from(currentTiles).forEach(tile => {
         tile.style.backgroundColor = newBgColor;
@@ -75,9 +78,34 @@ eraserBtn.addEventListener('click', () => {
     isErasing = !isErasing;
 
     if (isErasing) {
-        eraserBtn.style.backgroundColor = 'red'
+        eraserBtn.style.backgroundColor = 'lightgreen'
+        eraserBtn.style.color = 'black'
     } else {
-        eraserBtn.style.backgroundColor = 'blue'
+        eraserBtn.style.backgroundColor = 'red'
+        eraserBtn.style.color = 'white'
+        
+    };
+});
+
+const clearGridBtn = document.getElementById('clear-grid-btn');
+clearGridBtn.addEventListener('click', () => {
+    const currentTiles = document.getElementsByClassName('tile') ;
+    Array.from(currentTiles).forEach(tile => {
+        tile.style.backgroundColor = bgColor;
+    });
+});
+
+const randomModeBtn = document.getElementById('random-mode-btn')
+randomModeBtn.addEventListener('click', () => {
+    randomColorMode = !randomColorMode;
+
+    if (randomColorMode) {
+        randomModeBtn.style.background = "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,154,0,1) 10%, rgba(208,222,33,1) 20%,\
+                                                                 rgba(79,220,74,1) 30%, rgba(63,218,216,1) 40%, rgba(47,201,226,1) 50%,\
+                                                                 rgba(28,127,238,1) 60%, rgba(95,21,242,1) 70%, rgba(186,12,248,1) 80%,\
+                                                                 rgba(251,7,217,1) 90%, rgba(255,0,0,1) 100%)"
+    } else {
+        randomModeBtn.style.background = 'white'
     };
 });
 
@@ -96,16 +124,20 @@ function createTiles(gridHeight, squareWidth) {
     
         newSquare.addEventListener('mouseover', (e) => {
             if (e.buttons) {
-                newSquare.style.backgroundColor = isErasing ? bgColor : penColor;
+                newSquare.style.backgroundColor = isErasing ? bgColor : randomColorMode ? getRandomHexColor() : penColor;
             }
         });
     
         newSquare.addEventListener('mousedown', () => {
-            newSquare.style.backgroundColor = isErasing ? bgColor : penColor;
+            newSquare.style.backgroundColor = isErasing ? bgColor : randomColorMode ? getRandomHexColor() : penColor;
         });
     
         gridEl.appendChild(newSquare);
     
         gridHeight--;
     };
+};
+
+function getRandomHexColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
 };
